6f785ce2c3f0af461e1d9b96c8ec3ea744084f65
S-Lombardi <silo.lombardi@gmail.com>
2023-07-07 18:08:47 +0200

    Aggiunto file readme. Primo punto dell'esercizio: Definisco una classe Movie e dichiaro alsuo interno delle variabili

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+    //Definisco la classe Movie
+    class Movie{
+      //dichiaro delle variabili o attributi che avranno in comune le varie istanze di Movie
+        public $titolo;
+        public $regista;
+        public $genere;
+        public $voto;
+    
+    }
+    //ogni instanza è una COPIA della classe di partenza
+    //Hanno in comune caratteristiche/variabili espresse nella classe,ma
+    //sono due mondi separati 
+
+?>
---
+++ readme.md
@@ -0,0 +1,6 @@
+1-Definire una classe Movie;
+2=> all'interno della classe sono dichiarate delle variabili d'istanza;
+3=> all'interno della classe è definito un costruttore;
+4=> all'interno della classe è definito almeno un metodo;
+
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà. Non potete utilizzare il var_dump per stampare le proprietà dell'oggetto ma le dovete richiamare.
